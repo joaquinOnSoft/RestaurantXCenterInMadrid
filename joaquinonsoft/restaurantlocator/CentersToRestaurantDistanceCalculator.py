156a20d55a4904c9ebaca2681b0639e3f9e4a31c
JoaquinOnSoft <joaquingp@gmail.com>
2020-03-27 16:58:21 +0100

    Output file generated

--- joaquinonsoft/restaurantlocator/CentersToRestaurantDistanceCalculator.py
+++ joaquinonsoft/restaurantlocator/CentersToRestaurantDistanceCalculator.py
@@ -8,20 +8,28 @@ class CentersToRestaurantDistanceCalculator:
         self.restaurants = restaurants
 
     def calculate(self):
+        centers_extended_info = []
+
         for center in self.centers:
             min_distance = 1000000000
             nearest_restaurant = None
 
+            print(center["CODIGO CENTRO"], center["CENTRO"])
+
             for restaurant in self.restaurants:
-                coordinates_center = (float(center["lat"]), float(center["lng"]))
-                coordinates_restaurant = (float(restaurant["latitude"]), float(restaurant["longitude"]))
+                if center["lat"] is not None and center["lat"] != "":
+                    coordinates_center = (float(center["lat"]), float(center["lng"]))
+                else:
+                    coordinates_center = (0.0, 0.0)
 
-                print(coordinates_center)
-                print(coordinates_restaurant)
+                if restaurant["latitude"] is not None and restaurant["latitude"] != "":
+                    coordinates_restaurant = (float(restaurant["latitude"]), float(restaurant["longitude"]))
+                else:
+                    coordinates_restaurant = (0.0, 0.0)
 
+                # Getting distance between two points based on latitude/longitude
+                # https://stackoverflow.com/questions/19412462/getting-distance-between-two-points-based-on-latitude-longitude
                 dist = geopy.distance.vincenty(coordinates_center, coordinates_restaurant).kilometers
-
-                print(dist)
 
                 if dist < min_distance:
                     min_distance = dist
@@ -30,5 +38,24 @@ class CentersToRestaurantDistanceCalculator:
             if nearest_restaurant is not None:
                 center["restaurant"] = nearest_restaurant["address"]
                 center["distance"] = min_distance
+                centers_extended_info.append({
+                    "﻿AREA TERRITORIAL": center["﻿AREA TERRITORIAL"],
+                    "CODIGO CENTRO": center["CODIGO CENTRO"],
+                    "TIPO DE CENTRO": center["TIPO DE CENTRO"],
+                    "CENTRO": center["CENTRO"],
+                    "DOMICILIO": center["DOMICILIO"],
+                    "MUNICIPIO": center["MUNICIPIO"],
+                    "DISTRITO MUNICIPAL": center["DISTRITO MUNICIPAL"],
+                    "COD. POSTAL": center["COD. POSTAL"],
+                    "TELEFONO": center["TELEFONO"],
+                    "FAX": center["FAX"],
+                    "TITULARIDAD": center["TITULARIDAD"],
+                    "URL": center["URL"],
+                    "E-MAIL": center["E-MAIL"],
+                    "restaurant (nearest)": nearest_restaurant["name"],
+                    "restaurant address": nearest_restaurant["address"],
+                    "restaurant latitude": nearest_restaurant["latitude"],
+                    "restaurant longitude": nearest_restaurant["longitude"],
+                    "distance": min_distance})
 
-        return self.centers
+        return centers_extended_info
--- joaquinonsoft/restaurantlocator/MainDistanceCalculator.py
+++ joaquinonsoft/restaurantlocator/MainDistanceCalculator.py
@@ -3,27 +3,30 @@ import getopt
 import sys
 
 from joaquinonsoft.file.CSVReader import CSVReader
+from joaquinonsoft.file.CSVWriter import CSVWriter
 from joaquinonsoft.restaurantlocator.CentersToRestaurantDistanceCalculator import CentersToRestaurantDistanceCalculator
 
 
 def print_help():
     print('Calculate the distance of each center to the nearest restaurant')
     print('\nExecution:')
-    print('\tMainDistanceCalculator.py -o <output_file>')
+    print('\tMainDistanceCalculator.py -r <rodilla csv> -v <vips csv> -c <centers csv> -o <output_file>')
     print('where:')
     print('\t-h: Print this help')
     print('\t-r: (Mandatory) Rodilla\'s restaurants (CSV file)')
     print('\t-v: (Mandatory) VIPs restaurants (CSV file)')
     print('\t-c: (Mandatory) Center\'s list (CSV file with Schools, High schools)')
+    print('\t-o: (Mandatory) Output file')
 
 
 def main(argv):
     center_csv = None
     rodilla_csv = None
     vips_csv = None
+    output = None
 
     try:
-        opts, args = getopt.getopt(argv, "hc:r:v:", ["center=", "rodilla=", "vips="])
+        opts, args = getopt.getopt(argv, "hc:r:v:o:", ["center=", "rodilla=", "vips=", "output="])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
@@ -38,6 +41,8 @@ def main(argv):
             rodilla_csv = arg
         elif opt in ("-v", "--vips"):
             vips_csv = arg
+        elif opt in ("-o", "--output"):
+            output = arg
 
     if center_csv is not None and rodilla_csv is not None and vips_csv is not None:
         rodillas_reader = CSVReader(rodilla_csv, ";", csv.QUOTE_ALL)
@@ -52,7 +57,10 @@ def main(argv):
         restaurants = rodillas + vips
         calculator = CentersToRestaurantDistanceCalculator(centers, restaurants)
         centers = calculator.calculate()
-        print(centers)
+
+        if centers is not None and len(centers):
+            writer = CSVWriter(output, ";", csv.QUOTE_ALL)
+            writer.write(centers)
     else:
         print_help()
 
